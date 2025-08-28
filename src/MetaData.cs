168ec3b34395fbeaee69bddbdfd4d97eeaf011f5
D-Mass <dmitrynmaximov@gmail.com>
2025-08-28 20:50:16 +0300

    Retrieve cell pointer array

--- src/MetaData.cs
+++ src/MetaData.cs
@@ -9,9 +9,14 @@ namespace codecrafters_sqlite.src
 {
     internal class MetaData
     {
+        private const int _fileHeaderOffest = 16;
+        private const int _firstPageOffset = 100;
+        private const int _pageHeaderOffset = 8;
+
         private readonly string _path;
         private readonly int _pageSize;
         private int _tableCount;
+        private int[] cellPtrArray;
         internal FileStream databaseFile;
 
         internal MetaData(string path)
@@ -26,22 +31,32 @@ namespace codecrafters_sqlite.src
                 Console.Error.WriteLine(ex.Message);
             }
 
-            databaseFile.Seek(16, SeekOrigin.Begin);
-            byte[] buffer = new byte[2];
+            byte[] buffer = new byte[2]; // most of the data chunks are 2 bytes long
+
+            databaseFile.Seek(_fileHeaderOffest, SeekOrigin.Begin);
             databaseFile.Read(buffer, 0, 2);
             _pageSize = ReadUInt16BigEndian(buffer);
 
-            databaseFile.Seek(103, SeekOrigin.Begin);
+            databaseFile.Seek(_firstPageOffset + 3, SeekOrigin.Begin);
             databaseFile.Read(buffer, 0, 2);
             TableCount = ReadUInt16BigEndian(buffer);
-        }
 
+            cellPtrArray = new int[TableCount];
+            int arrayStartOffset = _firstPageOffset + _pageHeaderOffset;
+            int arrayIndexOffset = 0;
+            for (int i = 0; i < TableCount; i++)
+            {
+                arrayIndexOffset = i * 2; // 2 bytes per array element
+                databaseFile.Seek(arrayStartOffset + arrayIndexOffset, SeekOrigin.Begin);
+                databaseFile.Read(buffer, 0, 2);
+                cellPtrArray[i] = ReadUInt16BigEndian(buffer);
+            }            
+        }
         internal int TableCount
         {
             get { return _tableCount; }
             set { _tableCount = value; }
         }
-
         internal int PageSize
         {
             get { return _pageSize; }
