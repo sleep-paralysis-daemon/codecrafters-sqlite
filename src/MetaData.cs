b4f7fbc7e03ca7c27f961d55e9c9c7b40000d492
D-Mass <dmitrynmaximov@gmail.com>
2025-08-29 22:26:50 +0300

    Fix VarInt conversion

--- src/MetaData.cs
+++ src/MetaData.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Buffers.Binary.BinaryPrimitives;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("test-codecrafters-sqlite")]
 
 namespace codecrafters_sqlite.src
 {
-    internal class MetaData
+    public class MetaData
     {
         private const int _fileHeaderOffest = 16;
         private const int _firstPageOffset = 100;
@@ -67,7 +69,7 @@ namespace codecrafters_sqlite.src
         /// </summary>
         /// <param name="varIntOffset"> Starting offset from file's beginning </param>
         /// <returns> A tuple with VarInt and next byte offset </returns>
-        private (byte[], int) SliceOffVarInt(int varIntOffset)
+        internal (byte[], int) SliceOffVarInt(int varIntOffset)
         {
             int byteCount = 1;
             byte[] buffer = new byte[8];
@@ -76,8 +78,8 @@ namespace codecrafters_sqlite.src
                 databaseFile.Seek(varIntOffset + byteCount - 1, SeekOrigin.Begin);
                 databaseFile.Read(buffer, 0, 1);
                 byteCount++;
-                if (buffer[0] <= 0x7F) // bytes 0111_1111 and lower: most significant bit = 0 means there's no more bytes in VarInt
-                    break;
+                if (buffer[0] <= 0x7F || byteCount == 9) // bytes 0111_1111 and lower: most significant bit = 0 means there's no more bytes in VarInt.
+                    break;                              // max number of VarInt bytes is 9
             }
             databaseFile.Seek(varIntOffset, SeekOrigin.Begin);
             databaseFile.Read(buffer, 0, byteCount);
@@ -86,35 +88,58 @@ namespace codecrafters_sqlite.src
         }
 
 
-        private ulong ConvertVarInt(byte[] bytes)
+        internal ulong ConvertVarInt(byte[] bytes)
         {
             byte[] buffer = new byte[1];
             BitArray bits = new BitArray(bytes);
             Stack<bool> VarIntBits = new Stack<bool>();
             for (int i = 0; i < bits.Count; i++)
             {
-                if (i % 8 != 0) // transfer all bit except first bits of every byte
+                if (i % 8 != 0) // transfer alls bit except first bits of every byte
                 {
                     VarIntBits.Push(bits[i]);
                 }
             }
-            Stack<byte> cleanedBytes = new Stack<byte>();
+            Stack<byte> cleanBytes = new Stack<byte>();
             Stack<bool> cleanBits = new Stack<bool>();
             while (VarIntBits.Count > 0)
             {
-                c
+                cleanBits.Push(VarIntBits.Pop());
+                if (cleanBits.Count == 8)
+                {
+                    cleanBytes.Push(ConvertBitsToByte(cleanBits.ToArray()));
+                    cleanBits.Clear();
+                }
             }
-            ReadOnlySpan<byte> result = resultBytes.ToArray();
+            if (cleanBits.Count > 0)
+            {
+                while (cleanBits.Count < 8) cleanBits.Push(false);
+                cleanBytes.Push(ConvertBitsToByte(cleanBits.ToArray()));
+            }
+            while (cleanBytes.Count < 8)
+            {
+                cleanBytes.Push((byte)0);
+            }
+            ReadOnlySpan<byte> result = cleanBytes.ToArray();
             return ReadUInt64BigEndian(result);
         }
 
+        /// <summary>
+        /// Glues together 8 bits into a byte
+        /// </summary>
+        /// <param name="bits"></param>
+        /// <returns></returns>
         private byte ConvertBitsToByte(bool[] bits)
         {
+            //if (bits.Length != 8) throw new ArgumentOutOfRangeException("Need exactly 8 bits for a byte");
             byte result = 0;
             for (int i = 0; i < bits.Length; i++)
             {
                 if (bits[i])
+                {
+                    byte temp = (byte)(1 << (7 - i));
                     result |= (byte)(1 << (7 - i));
+                }                    
             }
             return result;
         }
