5db6018a5772e91fd36335fb5f9f41d9eef72228
D-Mass <dmitrynmaximov@gmail.com>
2025-09-13 23:58:12 +0300

    create simple select query syntax parser

---
+++ src/ASTBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_sqlite.src
+{
+    internal record SelectNode(List<string> columns, List<string> tables);
+    internal class ASTBuilder
+    {
+
+    }
+}
